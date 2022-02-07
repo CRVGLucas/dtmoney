514fb43035b74aa8d90c35ddf0f9d246c5cd87ef
Lucas <lucas@techdog.com.br>
2022-02-07 17:59:30 -0300

    arrumando o modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from 'react-modal'
-import { Container } from './styles'
+import { Container, TransactionTypeContainer, RadioBox } from './styles'
 import closeIMG from '../../assets/close.svg'
+import incomeICON from '../../assets/income.svg'
+import outcomeICON from '../../assets/outcome.svg'
+import { useState } from 'react'
 Modal.setAppElement('#root')
 
 interface NewTransactionModal {
@@ -9,6 +12,7 @@ interface NewTransactionModal {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModal) {
+    const [type, setType] = useState('deposit')
     return (
             <Modal isOpen={isOpen} onRequestClose={onRequestClose} overlayClassName='react-modal-overlay' className='react-modal-content'>
                 <Container >
@@ -16,6 +20,16 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
                     <h2>Cadastrar transação</h2>
                     <input placeholder="Titulo"/>
                     <input placeholder="Valor" type="number"/>
+                    <TransactionTypeContainer>
+                        <RadioBox  type="button"   onClick={() => setType('deposit')} isActive={type == 'deposit'} activeColor="green">
+                            <img src={incomeICON} />
+                            <span>Entrada</span>
+                        </RadioBox>
+                        <RadioBox  type="button" onClick={() => setType('widthdraw')}  isActive={type == 'withdraw'} activeColor="red">
+                            <img src={outcomeICON} />
+                            <span>Saída</span>
+                        </RadioBox>
+                    </TransactionTypeContainer>
                     <input placeholder="Categoria"/>
                     <button type="submit">Cadastrar</button>
                 </Container>
